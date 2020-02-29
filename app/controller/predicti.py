44154f8c5be6d1fefb1dac30be99b344e7c9c2df
suyfu <suyfu@yonyou.com>
2020-02-29 20:32:18 +0800

    添加罪名预测功能

--- app/controller/predicti.py
+++ app/controller/predicti.py
@@ -1,18 +1,19 @@
 from flask import  request, render_template, session, Blueprint
 from app.models import session
 from app.algorithm.sentencesimarly.sim_simhash import SimHaming
+from app.algorithm.Crimeclass.end import get_data
 # 创建蓝本对象
 predicti = Blueprint('predictions', __name__)
 
 
 @predicti.route('/prediction', methods=['GET', 'POST'])
 def prediction():
-    return render_template("crimepage.html")
+    return render_template("fanyi.html")
 
 @predicti.route('/prediction/crimclass',methods=['GET', 'POST'])
 def crim():
     keyword = request.form['keyword']
     print(keyword)
-    while (1):
-        sent = input(keyword)
-    return  render_template("crimeclass.html",label = label)
+    label = get_data(keyword)
+    print(label)
+    return  render_template("fanyi.html",label = label)
